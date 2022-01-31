e961205f56674da4a0eef4b119f0d76147fe2e16
shane carr <shcarr@microsoft.com>
2022-01-30 19:59:03 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express')
+let path = require('path')
+let app = express()
+let server = require('http').Server(app)
+
+// use the environment port if available, or default to 3000
+let port = process.env.port || 3003
+
+// serve static files from /public
+app.use(express.static('dist'))
+
+// create an endpoint which just returns the index.html page
+app.get('/', (req, res) => res.sendfile(path.join(__dirname, 'src/index.html')))
+
+// start the server
+server.listen(port, () => console.log(`server started on port ${port}`))
